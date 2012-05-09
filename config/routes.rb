d15a5d2ce3763a4fbd04e45945a807d5ae56ac1f
Julia Lin <lin.julia89@gmail.com>
2012-05-09 15:32:58 -0700

    fixed user model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 App::Application.routes.draw do
  
   get "stores/new"
-
   get "store/new"
 
   match '/', :to => "sessions#new"
@@ -9,22 +8,24 @@ App::Application.routes.draw do
   post "/users", :to => "users#create"
   post "/lists", :to => "lists#create"
   post "/sessions", :to => "sessions#create"
-  match "users/:id/:cur_list", :to => "users#show"
-  match "users/:id", :to => "users#show"
+  post "/stores", :to => "stores#create"
+  
+  match "/users/:id/:cur_list", :to => "users#show", :as => :default_home
+  match "/users/:id", :to => "users#show", :as => :home
   match "/signup",  :to => "users#new"
   match "/signin", :to => "sessions#new"
   match "/signout", :to => "sessions#destroy"
   match "/createlist", :to => "lists#new"
-  match "/deletelist/:id", :to => "lists#destroy"
+  match "/deletelist/:id", :to => "lists#destroy", :as => :delete_list
 
-  match "/addtolist/:list_id/:store_id", :to => "lists#add"
-  match "/removefromlist/:list_id/:store_id", :to => "lists_stores#destroy"
+  match "/addtolist/:list_id/:store_id", :to => "lists#add", :as => :add_to_list
+  match "/removefromlist/:list_id/:store_id", :to => "lists_stores#destroy", :as => :remove_from_list
 
   match "/addstore", :to => "stores#new"
 
-  post "/stores", :to => "stores#create"
+  
 
-  resources :sessions #, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   
   #resources :users
 
