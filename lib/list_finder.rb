adfd62cbe782185af5a54d983a72951ab7828689
Justin <jctanti@gmail.com>
2012-06-06 12:33:10 -0700

    Comments added to refactored files

--- lib/list_finder.rb
+++ lib/list_finder.rb
@@ -6,6 +6,7 @@ module ListFinder
 
 	module ClassMethods
 
+		#returns array of list objects representing lists that currently contain store denoted by store_id
 		def find_lists_by_curr_store(store_id, user_id)
 		 	t_curr_list_names = List.find(:all, 
                                   :conditions => ["list_users.user_id = ? and list_stores.store_id=?", user_id, store_id],
@@ -13,7 +14,7 @@ module ListFinder
                                   :select => "lists.name, lists.id")
 		end
 
-	#finds all lists belonging to the current user that are not a part of curr_lists, an array of list names
+		#finds all lists belonging to the current user that are not a part of curr_lists, an array of list names
 		def find_all_other_lists(curr_lists, user_id)
 			list_names = List.find(:all,
                                :conditions => ["lists.name NOT IN (?) and list_users.user_id = ?", curr_lists, user_id], 
@@ -21,7 +22,7 @@ module ListFinder
                                :select => "lists.name, lists.id")
 		end
 
-	#finds lists to display in views given ARRAY of list ids
+		#finds lists to display in views given ARRAY of list ids
 		def find_users_lists(user_id)
 			lists = List.find(:all,
                         :conditions => ["list_users.user_id = ?", user_id],
@@ -29,6 +30,7 @@ module ListFinder
                         :select => "lists.name, lists.id")
 		end
 
+		#returns list of lists that contain the store_id, given an array of lists to consider
 		def find_listers(lists, store_id)
 			results = List.find(:all, 
                             :conditions => ["lists.id in (?) and list_stores.store_id=?", lists, store_id],
--- lib/store_finder.rb
+++ lib/store_finder.rb
@@ -5,13 +5,15 @@ module StoreFinder
 	end
 
 	module ClassMethods
-		
+
+		#returns list of stores given array of store_ids
 		def find_stores_by_ids(store_ids)
-        stores = Store.find(:all,
+        	stores = Store.find(:all,
                             :conditions => ["id IN (?)", store_ids],
                             :select => "name, id")
     	end
 
+    	#probably don't need; can be done through association
   		def find_lists_stores(list_id)
 	  		stores = Store.find(:all,
                          	:conditions => ["list_stores.list_id = ?", list_id],
@@ -19,6 +21,7 @@ module StoreFinder
                          	:select => "stores.name, stores.id")
   		end
       	
+      	#simple search
     	def find_store_ids_by_fuzzy_match(search_string)
       		stores = Store.where("name like ?  or detail_info like ?", search_string, search_string).select("id").all
     	end
