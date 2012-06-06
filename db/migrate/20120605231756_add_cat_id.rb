de3094b8717ad36f1e98f9955f0af14dfb0b8d1f
Justin <jctanti@gmail.com>
2012-06-06 03:45:55 -0700

    Remove unnecessary migrations

--- db/migrate/20120605231756_add_cat_id.rb
+++
@@ -1,9 +0,0 @@
-class AddCatId < ActiveRecord::Migration
-  def up
-	add_column :tags, :category_id, :integer
-  end
-
-  def down
-	remove_column :tags, :category_id
-  end
-end
--- db/migrate/20120606061030_change_user_id_field.rb
+++
@@ -1,8 +0,0 @@
-class ChangeUserIdField < ActiveRecord::Migration
-  def up
-	add_column :tags, :user_id, :integer
-  end
-
-  def down
-  end
-end
