ad0343c9e6f4bcfce35bcd6f51e4110e3ada6089
unknown <Justin@GStarwind.(none)>
2012-06-08 14:12:49 -0700

    Storing Friends Lists in DB

--- app/business_logic/friend_logic.rb
+++ app/business_logic/friend_logic.rb
@@ -4,4 +4,16 @@ module FriendLogic
 		friend = Friend.find_or_create_by_user_id_and_friend_id(:user_id => user_id, :friend_id => friend_id)
 	end
 
+	#returns array of user objects representing the friends of User with user_id
+	def self.find_friends(user_id)
+		friend_objects = User.find(user_id).friends
+
+		friends = Array.new
+		friend_objects.each do |friend|
+			friends.push(User.find(friend.friend_id))
+		end
+
+		return friends
+	end
+
 end
